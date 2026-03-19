a3f4833d6b32f64c433d5906acfd095e026e95de
zoranjuras <cmundir@gmail.com>
2026-03-19 13:07:08 +0100

    Stage 4 tests passed.

--- Sorting Tool with Java/task/src/sorting/Main.java
+++ Sorting Tool with Java/task/src/sorting/Main.java
@@ -9,22 +9,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         List<String> inputLines = readInputLines(scanner);
 
-        if (hasArgument(args, "-sortIntegers")) {
-            sortAndCountIntegers(inputLines);
-            return;
-        }
+//        if (hasArgument(args, "-sortIntegers")) {
+//            sortAndCountIntegers(inputLines);
+//            return;
+//        }
 
         String dataType = getDataType(args);
+        String sortingType = getSortingType(args);
 
         if (dataType == null) {
-            System.out.println("Wrong usage, explanation blablabla...");
+            System.out.println("You must set value for -dataType");
             return;
         }
 
         switch (dataType) {
-            case "long" -> processLongs(inputLines);
-            case "word" -> processWords(inputLines);
-            case "line" -> processLines(inputLines);
+            case "long" -> processLongs(inputLines, sortingType);
+            case "word" -> processWords(inputLines, sortingType);
+            case "line" -> processLines(inputLines, sortingType);
             default -> System.out.println("Wrong data type!");
         }
     }
@@ -40,9 +41,20 @@ public class Main {
         return dataType;
     }
 
-    private static boolean hasArgument(String[] args, String argument) {
-        return Arrays.asList(args).contains(argument);
+    private static String getSortingType(String[] args) {
+        String sortingType = "natural";
+        for (int i = 0; i < args.length - 1; i++) {
+            if (args[i].equals("-sortingType")) {
+                sortingType = args[i + 1];
+                break;
+            }
+        }
+        return sortingType;
     }
+
+//    private static boolean hasArgument(String[] args, String argument) {
+//        return Arrays.asList(args).contains(argument);
+//    }
 
     private static void sortAndCountIntegers(List<String> inputLines) {
 
@@ -56,43 +68,142 @@ public class Main {
         }
     }
 
-    private static void processLongs(List<String> inputLines) {
-
-        List<Long> numbersList = parseNumbers(inputLines);
-        numbersList.sort(null);
-        int numbersCount = numbersList.size();
-        long maxNumber = numbersList.getLast();
-        int occurrences = Collections.frequency(numbersList, maxNumber);
-        int percentage = (int) Math.round((double) occurrences / numbersCount * 100);
-
-        System.out.printf("Total numbers: %d.%n", numbersCount);
-        System.out.printf("The greatest number: %s (%d time(s), %d%%).%n", maxNumber, occurrences, percentage);
-    }
-
-    private static void processWords(List<String> inputLines) {
+    private static void sortAndCountWords(List<String> inputLines) {
 
         List<String> wordsList = parseWords(inputLines);
-        wordsList.sort(Comparator.comparingInt(String::length));
+        wordsList.sort(null);
         int wordsCount = wordsList.size();
-        String maxLengthWord = wordsList.getLast();
-        int wordOccurrences = Collections.frequency(wordsList, maxLengthWord);
-        int wordPercentage = (int) Math.round((double) wordOccurrences / wordsCount * 100);
-
         System.out.printf("Total words: %d.%n", wordsCount);
-        System.out.printf("The longest word: %s (%d time(s), %d%%).%n", maxLengthWord, wordOccurrences, wordPercentage);
+        System.out.print("Sorted data:");
+        for (String word : wordsList) {
+            System.out.print(" " + word);
+        }
     }
 
-    private static void processLines(List<String> inputLines) {
+    private static void sortAndCountLines(List<String> inputLines) {
 
         List<String> linesList = new ArrayList<>(inputLines);
-        linesList.sort(Comparator.comparingInt(String::length));
+        linesList.sort(null);
         int linesCount = linesList.size();
-        String maxLengthLine = linesList.getLast();
-        int linesOccurrences = Collections.frequency(linesList, maxLengthLine);
-        int linesPercentage = (int) Math.round((double) linesOccurrences / linesCount * 100);
+        System.out.printf("Total lines: %d.%n", linesCount);
+        System.out.print("Sorted data:");
+        for (String line : linesList) {
+            System.out.print(" " + line);
+        }
+    }
+
+    private static void sortByCountAndCountIntegers(List<String> inputLines) {
+
+        List<Long> numbersList = parseNumbers(inputLines);
+        int numbersCount = numbersList.size();
+
+        Map<Long, Integer> numbersMap = new HashMap<>();
+
+        for (Long number : numbersList) {
+            numbersMap.put(number, numbersMap.getOrDefault(number, 0) + 1);
+        }
+
+        List<Map.Entry<Long, Integer>> entries = new ArrayList<>(numbersMap.entrySet());
+
+        entries.sort(
+                Comparator.comparing(Map.Entry<Long, Integer>::getValue)
+                        .thenComparing(Map.Entry<Long, Integer>::getKey)
+        );
+
+        System.out.printf("Total numbers: %d.%n", numbersCount);
+
+        for (Map.Entry<Long, Integer> entry : entries) {
+            long number = entry.getKey();
+            int count = entry.getValue();
+            int percentage = (int) Math.round((double) count / numbersCount * 100);
+
+            System.out.println(number + ": " + count + " time(s), " + percentage + "%");
+        }
+    }
+
+    private static void sortByCountAndCountWords(List<String> inputLines) {
+
+        List<String> wordsList = parseWords(inputLines);
+        int wordsCount = wordsList.size();
+
+        Map<String, Integer> wordsMap = new HashMap<>();
+
+        for (String word : wordsList) {
+            wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(wordsMap.entrySet());
+
+        entries.sort(
+                Comparator.comparing(Map.Entry<String, Integer>::getValue)
+                        .thenComparing(Map.Entry<String, Integer>::getKey)
+        );
+
+        System.out.printf("Total words: %d.%n", wordsCount);
+
+        for (Map.Entry<String, Integer> entry : entries) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+            int percentage = (int) Math.round((double) count / wordsCount * 100);
+
+            System.out.println(word + ": " + count + " time(s), " + percentage + "%");
+        }
+    }
+
+    private static void sortByCountAndCountLines(List<String> inputLines) {
+
+        List<String> linesList = new ArrayList<>(inputLines);
+        int linesCount = linesList.size();
+
+        Map<String, Integer> linesMap = new HashMap<>();
+
+        for (String line : linesList) {
+            linesMap.put(line, linesMap.getOrDefault(line, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(linesMap.entrySet());
+
+        entries.sort(
+                Comparator.comparing(Map.Entry<String, Integer>::getValue)
+                        .thenComparing(Map.Entry<String, Integer>::getKey)
+        );
 
         System.out.printf("Total lines: %d.%n", linesCount);
-        System.out.printf("The longest line:%n%s%n(%d time(s), %d%%).%n", maxLengthLine, linesOccurrences, linesPercentage);
+
+        for (Map.Entry<String, Integer> entry : entries) {
+            String line = entry.getKey();
+            int count = entry.getValue();
+            int percentage = (int) Math.round((double) count / linesCount * 100);
+
+            System.out.println(line + ": " + count + " time(s), " + percentage + "%");
+        }
+    }
+
+    private static void processLongs(List<String> inputLines, String sortingType) {
+
+        if (sortingType.equals("natural")) {
+            sortAndCountIntegers(inputLines);
+            return;
+        }
+        sortByCountAndCountIntegers(inputLines);
+    }
+
+    private static void processWords(List<String> inputLines, String sortingType) {
+
+        if (sortingType.equals("natural")) {
+            sortAndCountWords(inputLines);
+            return;
+        }
+        sortByCountAndCountWords(inputLines);
+    }
+
+    private static void processLines(List<String> inputLines, String sortingType) {
+
+        if (sortingType.equals("natural")) {
+            sortAndCountLines(inputLines);
+            return;
+        }
+        sortByCountAndCountLines(inputLines);
     }
 
     private static List<String> extractTokens(List<String> lines) {
